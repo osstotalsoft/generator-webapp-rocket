b56b959c46d4804d778cd2bb939230119721b1bd
Alexandra Capatina <capatinaalexandra@gmail.com>
2020-08-20 12:59:34 +0300

    add more tests

--- __tests__/app.js
+++ __tests__/app.js
@@ -1,38 +1,54 @@
-'use strict';
-const path = require('path');
-const assert = require('yeoman-assert');
-const helpers = require('yeoman-test');
-const rimraf = require('rimraf');
+'use strict'
+const path = require('path')
+const assert = require('yeoman-assert')
+const helpers = require('yeoman-test')
+const rimraf = require('rimraf')
 
 describe('generator-webapp-rocket:app', () => {
-  const projectName = 'test';
+  const projectName = 'test'
+  const gqlAddress = 'localhost:4000'
 
-  beforeAll(() => {
+  beforeEach(() => {
     return helpers
       .run(path.join(__dirname, '../generators/app'))
       .inDir(path.join(__dirname, 'tmp'))
       .withPrompts({
         projectName,
         projectDescriptionName: 'test description',
-        gqlAddress: 'localhost:4000',
+        gqlAddress,
         theme: 'default',
-        addHelm: false,
+        addHelm: true,
         identityClientId: 'id',
         identityScope: '',
         identityAuthority: 'localhost:5000',
         packageManager: 'npm'
-      }).then(runContext => {
-        return runContext
       })
-  });
+  })
 
-  afterAll(() => {
-    rimraf.sync(path.join(__dirname, 'tmp'));
-  });
+  afterEach(async () => {
+    await rimraf(path.join(__dirname, 'tmp'))
+  })
 
   it('create component with given name', () => {
     const resPath = path.join(__dirname, `tmp/${projectName}/src/index.js`)
-    assert.file(resPath);
-  });
+    assert.file(resPath)
+  })
 
-});
+  it('project has given name', () => {
+    assert.fileContent(
+      path.join(__dirname, `tmp/${projectName}/package.json`),
+      `"name": "${projectName}"`
+    )
+  })
+
+  it('gql address is configured', () => {
+    assert.fileContent(
+      path.join(__dirname, `tmp/${projectName}/.env`),
+      `REACT_APP_GQL=${gqlAddress}`
+    )
+  })
+
+  it('helm files are added when addHelm option is true', () => {
+    assert.file(path.join(__dirname, `tmp/${projectName}/helm`));
+  });
+})
