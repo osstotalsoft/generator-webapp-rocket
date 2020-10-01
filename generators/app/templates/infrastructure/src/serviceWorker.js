898f9b8d4f93f1eb8c33c5172889e8f890eb6d87
Alexandra <capatinaalexandra@gmail.com>
2020-10-01 14:40:55 +0300

    small fix string concatenation

--- generators/app/templates/infrastructure/src/serviceWorker.js
+++ generators/app/templates/infrastructure/src/serviceWorker.js
@@ -42,8 +42,7 @@ export function register(config) {
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
           console.log(
-            'This web app is being served cache-first by a service ' +
-              'worker. To learn more, visit https://bit.ly/CRA-PWA'
+            'This web app is being served cache-first by a service worker. To learn more, visit https://bit.ly/CRA-PWA'
           );
         });
       } else {
@@ -70,8 +69,7 @@ function registerValidSW(swUrl, config) {
               // but the previous service worker will still serve the older
               // content until all client tabs are closed.
               console.log(
-                'New content is available and will be used when all ' +
-                  'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
+                'New content is available and will be used when all tabs for this page are closed. See https://bit.ly/CRA-PWA.'
               );
 
               // Execute callback
