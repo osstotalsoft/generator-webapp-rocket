05a6e90ddc32394c6cda5e413164a503fd2b7b87
Costinel Diaconita <cdiaconita@totalsoft.ro>
2021-02-25 10:50:28 +0200

    merge useUserData and useRights hooks

--- generators/app/templates/infrastructure/src/hooks/rights.js
+++ generators/app/templates/infrastructure/src/hooks/rights.js
@@ -1,46 +1,41 @@
 import permissions from 'constants/permissions'
 import { gql } from '@apollo/client'
 import { useQueryWithErrorHandling } from './errorHandling'
-import { useReactOidc } from '@axa-fr/react-oidc-context';
-import { includes } from 'ramda';
+import { useReactOidc } from '@axa-fr/react-oidc-context'
+import { includes } from 'ramda'
 
-const { viewSettings} = permissions
+const { viewSettings } = permissions
 const GET_USER_DATA = gql`
-    query userData($externalId: ID!){
-        userData(externalId: $externalId){
-            id
-            userName
-            rights
-        }
+  query userData($externalId: ID!) {
+    userData(externalId: $externalId) {
+      id
+      userName
+      rights
     }
+  }
 `
 
 export function useUserData() {
-    const { oidcUser } = useReactOidc();
-    const externalUserId = oidcUser?.profile?.sub;
+  const { oidcUser } = useReactOidc()
+  const externalUserId = oidcUser?.profile?.sub
 
-    const { data, ...res } = useQueryWithErrorHandling(GET_USER_DATA, {
-        variables: {
-            externalId: externalUserId
-        },
-        skip: !externalUserId
-    })
-    return { ...res, userData: data?.userData };
-}
+  const { data, ...res } = useQueryWithErrorHandling(GET_USER_DATA, {
+    variables: {
+      externalId: externalUserId
+    },
+    skip: !externalUserId
+  })
+  const rights = data?.userData?.rights
 
-export const useRights = () => {
-    const { oidcUser } = useReactOidc();
-    const externalUserId = oidcUser?.profile?.sub;
-
-    const { data } = useQueryWithErrorHandling(GET_USER_DATA, {
-        variables: {
-            externalId: externalUserId
-        },
-        skip: !externalUserId
-    })
-    const rights = data?.userData?.rights
-
-    return rights ? {
-        canViewSettings: includes(viewSettings, rights)
-    } : {}
+  return {
+    ...res,
+    userData: {
+      ...data?.userData,
+      rights: rights
+        ? {
+            canViewSettings: includes(viewSettings, rights)
+          }
+        : {}
+    }
+  }
 }
