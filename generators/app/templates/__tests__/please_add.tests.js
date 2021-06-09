7ffed6986c6c0bc846545e0f78ce2e23da871512
Elena Dumitrescu <edumitrescu@totalsoftostan.onmicrosoft.com>
2021-06-09 11:11:50 +0300

    test run

--- generators/app/templates/__tests__/please_add.tests.js
+++ generators/app/templates/__tests__/please_add.tests.js
@@ -2,6 +2,6 @@ describe('root tests suite:', () => {
   it('using |> operator should not have undesired side-effects: ', () => {
     const add = x => y => x + y
     const result = 1 |> add(2)
-    expect(result).toBe(6)
+    expect(result).toBe(7)
   })
 })
