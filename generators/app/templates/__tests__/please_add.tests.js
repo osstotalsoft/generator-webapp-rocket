85dc1b10e0a98aadb468dfcb0df1cf6480282189
Elena Dumitrescu <edumitrescu@totalsoftostan.onmicrosoft.com>
2021-06-09 12:43:43 +0300

    add husky

--- generators/app/templates/__tests__/please_add.tests.js
+++
@@ -1,7 +0,0 @@
-describe('root tests suite:', () => {
-  it('using |> operator should not have undesired side-effects: ', () => {
-    const add = x => y => x + y
-    const result = 1 |> add(2)
-    expect(result).toBe(7)
-  })
-})
