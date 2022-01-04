e8031d87928c899fcb56d6f0286bd47ea54356c2
Alexandra Capatina <46926675+alexandra-c@users.noreply.github.com>
2022-01-04 16:09:49 +0200

    cleanup packages and upgrade node

--- generators/app/templates/infrastructure/src/utils/testingUtils/createClient.js
+++ generators/app/templates/infrastructure/src/utils/testingUtils/createClient.js
@@ -1,13 +1,9 @@
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloClient } from 'apollo-client';
-import { MockLink } from 'apollo-link-mock';
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { MockLink } from 'apollo-link-mock'
 
-export default function createClient({
-    link,
-    mocks = [],
-}) {
-    return new ApolloClient({
-        cache: new InMemoryCache(),
-        link: link ? link : new MockLink(mocks),
-    });
+export default function createClient({ link, mocks = [] }) {
+  return new ApolloClient({
+    cache: new InMemoryCache(),
+    link: link ? link : new MockLink(mocks)
+  })
 }
