ef684b5a91f8e35a61e6a96a72d4a6a9bd1a3e4e
Costinel Diaconita <cdiaconita@totalsoft.ro>
2022-01-17 12:17:56 +0200

    update react scripts to v5

--- generators/app/templates/infrastructure/config-overrides.js
+++ generators/app/templates/infrastructure/config-overrides.js
@@ -1,4 +1,7 @@
 /* config-overrides.js */
-const { override, addBabelPlugin } = require("customize-cra");
+const { override, addBabelPlugin } = require('customize-cra')
 
-module.exports = override(addBabelPlugin(["@babel/plugin-proposal-pipeline-operator", { proposal: "minimal" }]));
+module.exports = override(addBabelPlugin(['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }]), config => {
+    config.ignoreWarnings = [/Failed to parse source map/]
+    return config
+})
