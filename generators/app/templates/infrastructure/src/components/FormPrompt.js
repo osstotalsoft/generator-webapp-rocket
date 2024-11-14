7e674461ea06c09353c8d2b96b2743bbf800e9f2
Elena.Dumitrescu@totalsoft.ro <elena.dumitrescu@totalsoft.ro>
2024-11-14 12:28:41 +0200

    upgraded react-router-dom package to latest

---
+++ generators/app/templates/infrastructure/src/components/FormPrompt.js
@@ -0,0 +1,35 @@
+import { useEffect, useCallback, useRef } from 'react'
+import PropTypes from 'prop-types'
+import { useBlocker } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
+import { equals } from 'ramda'
+
+const usePrompt = onLocationChange => {
+  const blocker = useBlocker(onLocationChange)
+  const prevState = useRef(blocker.state)
+
+  useEffect(() => {
+    if (equals(blocker.state, 'blocked')) {
+      blocker.reset()
+    }
+    prevState.current = blocker.state
+  }, [blocker])
+}
+
+const FormPrompt = ({ hasUnsavedChanges }) => {
+  const { t } = useTranslation()
+  const onLocationChange = useCallback(() => {
+    if (hasUnsavedChanges) {
+      return !window.confirm(t('General.LeavingWithoutSaving'))
+    }
+    return false
+  }, [hasUnsavedChanges, t])
+
+  usePrompt(onLocationChange)
+}
+
+FormPrompt.propTypes = {
+  hasUnsavedChanges: PropTypes.bool
+}
+
+export default FormPrompt
